e8e3e49cf44d8de81958c703f64024bd300d819a
Jenny Gao <jgao658@gmail.com>
2018-02-07 14:30:10 -0500

    working

--- canvas.js
+++ canvas.js
@@ -3,21 +3,38 @@ var stopButton=document.getElementById("stop")
 var canvas=document.getElementById("slate");
 var ctx=canvas.getContext("2d");
 
+var requestID;
+
 var stopIt=function(){
     window.cancelAnimationFrame(requestID);
 };
 
 var animate=function(){
     var r=10;
+    var tooBig=false;
+    var tooSmall=false;
     window.cancelAnimationFrame(requestID);
     var circle=function(){
-	requestID=window.requestAnimationFrame(circle);
+	ctx.clearRect(0,0,500,500);
 	ctx.beginPath();
-	ctx.arc(250,250,r,0,2*Math.PI);
+	if(r==250){
+	    tooBig=true;
+	}
+	if(!tooBig){
+	    ctx.arc(250,250,r,0,2*Math.PI);
+	    r++;
+	}
+	else{
+	    r--;
+	    ctx.arc(250,250,r,0,2*Math.PI);
+	    if(r==10){
+		tooBig=false;
+	    }
+	}
 	ctx.fillStyle="blue";
 	ctx.fill();
 	ctx.stroke();
-	r++;
+	requestID=window.requestAnimationFrame(circle);
     };
     circle();
 };
